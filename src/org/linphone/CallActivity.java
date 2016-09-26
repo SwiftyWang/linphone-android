e09c4497618ba4cbcebb64ae40bb9dff07fe997f
Erwan Croze <erwan.croze@belledonne-communications.com>
2016-09-26 13:24:07 +0200

    Fixed check permission on update

--- src/org/linphone/CallActivity.java
+++ src/org/linphone/CallActivity.java
@@ -1159,11 +1159,7 @@ public class CallActivity extends Activity implements OnClickListener, SensorEve
 				if (camera == PackageManager.PERMISSION_GRANTED) {
 					CallActivity.instance().acceptCallUpdate(true);
 				} else {
-					if (LinphonePreferences.instance().firstTimeAskingForPermission(Manifest.permission.CAMERA) || ActivityCompat.shouldShowRequestPermissionRationale(CallActivity.this, Manifest.permission.CAMERA)) {
-						checkAndRequestPermission(Manifest.permission.CAMERA, PERMISSIONS_REQUEST_CAMERA);
-					} else {
-						CallActivity.instance().acceptCallUpdate(false);
-					}
+					checkAndRequestPermission(Manifest.permission.CAMERA, PERMISSIONS_REQUEST_CAMERA);
 				}
 
 				dialog.dismiss();
